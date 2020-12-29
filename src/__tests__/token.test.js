f690d5e9d0319cea0d80c083da9747968e153348
Max Bogaers <mbogaers@studnet.avans.nl>
2020-12-29 17:19:36 +0100

    added tests for token

---
+++ src/__tests__/token.test.js
@@ -0,0 +1,58 @@
+const {signToken,returnTokenUser, validateToken} = require("../services/token");
+describe("Auth Service", () => {
+  test("Should verify Valid Token", () => {
+    const token = signToken({name:"test"})
+    let result;
+
+    result = validateToken(token);
+
+    expect(result).toEqual(true);
+  })
+  test("Shouldnt verify invalid Token", () => {
+    const token = "obviously not a token"
+    let result;
+
+
+    result = validateToken(token);
+
+    expect(result).toEqual(false);
+  })
+
+  test("Shouldnt verify null Token", () => {
+    const token = null;
+    let result;
+
+
+    result = validateToken(token);
+
+    expect(result).toEqual(false);
+  })
+
+  test("Should get object from valid token", () => {
+    const token = signToken({_id:"Not Syke" ,name:"test"});
+    let result;
+
+
+    result =  returnTokenUser(token)
+    expect(result.user).toEqual({_id:"Not Syke" ,name:"test"});
+  })
+
+  test("Should get fake object from invalid token", () => {
+    const token = "obviously not a token"
+    let result;
+
+
+    result =  returnTokenUser(token)
+    expect(result.user).toBeUndefined();
+  })
+
+  test("Shouldnt get object from null token", () => {
+    const token = null
+    let result;
+
+
+    result =  returnTokenUser(token)
+    expect(result.user).toBeUndefined();
+  })
+
+});
